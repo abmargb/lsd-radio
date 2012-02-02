ab778652e8e69f56171d2d8c9b80bfde038653f4
abmargb <abmargb@gmail.com>
2012-02-02 15:29:50 -0300

    Fixed accent marks. For real this time.

--- server/server.py
+++ server/server.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import logging
 import simplejson, urllib
 import os
 import utils
+import string
+import random
+
 from config import RADIO_ROOT, SERVER_URL
 
 app = Flask(__name__)
+unlike_votes = []
 
 @app.route('/vote', methods=["POST"])
 def vote():
     
     vote = request.form["vote"]
     
-    url = "http://gdata.youtube.com/feeds/api/videos?q=%s&max-results=1&v=2&alt=jsonc" % urllib.quote_plus(vote)
+    params = urllib.urlencode({'q': vote, 'max-results': '1', 'v': '2', 'alt': 'jsonc'})
+    
+    url = "http://gdata.youtube.com/feeds/api/videos?%s" % params
     result = simplejson.load(urllib.urlopen(url))
     video_id = result['data']['items'][0]['id']
     
@@ -20,9 +26,35 @@ def vote():
     
     return 'OK'
 
+@app.route('/unlike')
+def unlike():
+    token = request.cookies.get('token')
+    
+    if (not token in unlike_votes) :
+        unlike_votes.append(token)
+        
+    return 'OK'
+
 @app.route('/')
 def index():
-    return render_template("index.html", server_url=SERVER_URL)
+    
+    token = request.cookies.get('token')
+    resp = make_response(render_template("index.html", server_url=SERVER_URL))
+    
+    if (not token) :
+        resp.set_cookie('token', id_generator())
+    
+    return resp
+
+def update_song():
+    if (updated) :
+        del unlike_votes[0:len(unlike_votes)]
+
+def get_connections():
+    return 5
+
+def id_generator(size=8, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
